dbb69117bb1ceae705d7af6975916e80858b3b00
Leon Thorne <ldthorne@brandeis.edu>
2016-01-21 19:01:47 -0500

    Revert "haveafter hours searching high and low through the facebook dom, i found the element that contains a facebook 'card'. also, basic stuff for chrome extension"
    This reverts commit b65d95c2710a8a2af2a31e5bc1ecd90047c4f960.

--- Facebook_files/eoermEBmIjd.js
+++
@@ -1,5 +0,0 @@
-/*!CK:1842419302!*//*1453086741,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["HidLU"]); }
-
-__d('canUseReactEditor',['UserAgent'],function a(b,c,d,e,f,g,h){'use strict';if(c.__markCompiled)c.__markCompiled();var i=typeof b.getSelection==='function',j=i&&((h.isPlatform('iOS')||h.isPlatform('Linux')||h.isPlatform('Mac OS X')||h.isPlatform('Windows')||h.isPlatform('Chrome OS'))&&(h.isBrowser('Chrome')||h.isBrowser('Edge >= 12')||h.isBrowser('Firefox >= 16')||h.isBrowser('IE >= 9')||h.isBrowser('Opera >= 12')||h.isBrowser('Mobile Safari >= 6')||h.isBrowser('Safari >= 5')));function k(){return j;}f.exports=k;},null);
--- Facebook_files/q20eaBYX6RL.js
+++
@@ -1,5 +0,0 @@
-/*!CK:74183250!*//*1453408733,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["rvZC5"]); }
-
-__d('UFIShareNowMenu.react',['ContextualDialogArrow','ContextualLayerAsyncRelative','ContextualLayerAutoFlip','PopoverAsyncMenu.react','React','ShareNowController','UFIShareLink.react'],function a(b,c,d,e,f,g,h,i,j,k,l,m,n){if(c.__markCompiled)c.__markCompiled();var o=l.PropTypes,p=l.createClass({displayName:'UFIShareNowMenu',propTypes:{focusOnInit:o.bool,openOnInit:o.bool},_mountWithMenu:function(q){if(!q)return;var r=q.getPopoverMenu();m.initMenu(r);if(this.props.focusOnInit)r.getTriggerElem().focus();if(this.props.openOnInit)r.getTriggerElem().click();},render:function(){var q=this.props,r=q.endpoint,s=q.endpointData,t=q.onShow,u=q.onHide;return (l.createElement(k,{alignv:'baseline',endpoint:r,endpointData:s,onShow:t,onHide:u,layerBehaviors:[j,h,i],ref:this._mountWithMenu},l.createElement(n,{href:'#'})));}});f.exports=p;},null);
