7eea4e7cb882d3d0435e82de6cb0e8998fab3cc7
Leon Thorne <ldthorne@brandeis.edu>
2016-01-23 22:56:50 -0500

    mostly done; having some weird jquery issues

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,51 @@
-$(".UFILikeLink").remove();
-
-var currentHeight = $("#globalContainer").height();
-
-// console.log("UCw", $(".userContentWrapper"))
-// console.log("links", $(".profileLink"))
-$(document).on('scroll', function(event){
-	var heightRn = $("#globalContainer").height()
-	console.log(currentHeight === heightRn)
-	if(currentHeight !== $("#globalContainer").height()){
-		deleteAllDomLikes()
-		currentHeight=heightRn;
-	}
+var people;
+$(document).ready(function(){
+	chrome.storage.local.get(function(values){
+        people = JSON.parse(values.namesArr)
+        console.log(people)
+    	deleteAllDomLikes(people)
+    })
 })
 
+var currentHeight = $("#globalContainer").height();
+$(document).on('scroll', function(event) {
+    var heightRn = $("#globalContainer").height();
+    if (currentHeight !== $("#globalContainer").height()) {
+    	console.log('hello no people');
+    	chrome.storage.local.get(function(values){
+	        people = JSON.parse(values.namesArr)
+	    	deleteAllDomLikes(people)
+        	currentHeight = heightRn;
+	    })
+    }
+})
 
-function deleteAllDomLikes(){
-	var arr = [].slice.call($("._5pbw"))
-	arr.forEach(function(card){
-		console.log(card.innerText);
-	});
-	// $(".UFILikeLink").remove();
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+    	console.log("people added or removed from list")
+        chrome.storage.local.get(function(values){
+	        people = JSON.parse(values.namesArr)
+	        console.log("FROM CONTENT", people)
+	        deleteAllDomLikes(people)
+	    })
+    }
+);
+
+
+function deleteAllDomLikes(peopleToDelete) {
+    console.log(peopleToDelete)
+    peopleToDelete.forEach(function(person) {
+        person = person.toLowerCase();
+        $("._5pbw").each(function() {
+            var titleOfPost = $(this);
+            var textOfPost = titleOfPost[0].innerText.toLowerCase();
+            if (textOfPost.indexOf(person) > -1) {
+                console.log("true")
+                var wholePostCard = titleOfPost.parents(".userContentWrapper");
+                var likeButton = wholePostCard.find("._42nr").find(".UFILikeLink");
+                likeButton.remove();
+                console.log("removing because ", person)
+            }
+        });
+    })
 }
--- popup.js
+++ popup.js
@@ -1,118 +1,116 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+$(document).ready(function() {
+    getListOfPeople();
+    $("#submit-button").click(function() {
+        saveChanges();
     });
-  });
+})
+
+$(document).on('click', 'button', function(e) {
+    var idx = $(e)[0].currentTarget.id;
+    deletePersonFromList(idx);
+    console.log($("#"+idx))
+    $($(e)[0].currentTarget).remove()
+    $("#"+idx).remove()
 });
+
+
+function deletePersonFromList(index){
+    console.log("delete person from list being called on index", index)
+    chrome.storage.local.get(function(values){
+        console.log("DPFL getting vals", values)
+        namesArr = JSON.parse(values.namesArr);
+        namesArr.splice(index,1);
+        console.log("DPFL modified values", namesArr)
+
+        chrome.storage.local.set({
+               'namesArr': JSON.stringify(namesArr)
+            },
+            function() {
+                // Notify that we saved.
+                getListOfPeople();
+                $("#js-response").empty()
+                $("#js-response").append('Removed name from arr and saved');
+            }
+        );
+    })
+}
+
+function getListOfPeople() {
+    console.log("GLOP being called")
+    $("#list").empty();
+    chrome.storage.local.get(function(values) {
+        console.log("GLOP getting vals", values)
+        if (values.namesArr) {
+            JSON.parse(values.namesArr).forEach(function(name, index) {
+                console.log("appending shit", index, name)
+                $("#list").append("<p id="+index+">"+name+"<br><br>");
+                $("#buttons").append('<button type="button" class="btn btn-circular btn-default delete-btns" id="'+index+'" aria-label="Left Align"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span><br><br>')
+            });
+        }
+        sendMessageToContentScript();
+    })
+}
+
+function sendMessageToContentScript() {
+    console.log("sending message to content script")
+    chrome.storage.local.get(function(values){
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function(tabs) {
+            //tabs is an array even if there is only one result
+            var message = values.namesArr;
+            console.log("sent message to content script", message)
+        });
+    })
+}
+
+function saveChanges() {
+    console.log("saving changes")
+    $("#js-response").empty();
+    // // Get a value saved in a form.
+    var theValue = $("#name-entry").val()
+        // // Check that there's some code there.
+    if (!theValue) {
+        $("#js-response").append('No name specified!');
+        // return;
+    } else {
+        chrome.storage.local.get(function(values) {
+            if (values.namesArr) {
+                values.namesArr = JSON.parse(values.namesArr)
+                console.log(values.namesArr)
+                if (values.namesArr.indexOf(theValue) !== -1) {
+                    $("#js-response").append('name already in arr')
+                } else {
+                    values.namesArr.push(theValue);
+                    chrome.storage.local.set({
+                            'namesArr': JSON.stringify(values.namesArr)
+                        },
+                        function() {
+                            // Notify that we saved.
+                            getListOfPeople();
+                            $("#js-response").append('Pushed new name to arr and saved');
+                        }
+                    );
+                }
+            } else {
+                console.log('being called')
+                var namesArr = [];
+                var name = theValue;
+                namesArr.push(theValue)
+                chrome.storage.local.set({
+                        'namesArr': JSON.stringify(namesArr)
+                    },
+                    function() {
+                        getListOfPeople();
+                        $("#js-response").append('Created arr and saved');
+                    }
+                );
+            }
+        })
+    }
+
+
+    // // Save it using the Chrome extension storage API.
+
+}
